732e978ac31109fc0241c951d1f84747527c49ad
Nengzhuo Cai <cainengzhuo@gmail.com>
2022-07-18 03:27:16 -0400

    Canvas API 2

--- src/engine/Engines.ts
+++ src/engine/Engines.ts
@@ -1,6 +1,7 @@
 import type { ProjectRT } from "./ProjectRT";
+import type {canvas} from "./CanvasAPI"
 import UnipackRT from "./Unipack/UnipackRT";
 
-export let projectEngines : {[name:string]: {(api:object):ProjectRT};} = {
-  "Unipack": (api:object) => {return new UnipackRT(api);}
+export let projectEngines : {[name:string]: {(api:canvas):ProjectRT};} = {
+  "Unipack": (api:canvas) => {return new UnipackRT(api);}
 };
