f1b42a4d02a165b6c94864691a11113b8d3533cc
Nengzhuo Cai <Null@203.io>
2022-08-21 05:17:30 -0400

    Beginning of ULP engine

--- src/engine/Engines.ts
+++ src/engine/Engines.ts
@@ -1,7 +1,9 @@
 import type { ProjectRT } from "./ProjectRT";
 import type {canvas} from "./CanvasAPI"
 import UnipackRT from "./Unipack/UnipackRT";
+import ULP from "./ULP/ULP";
 
 export let projectEngines : {[name:string]: {(api:canvas):ProjectRT};} = {
+  "ULP": (api:canvas) => {return new ULP(api);},
   "Unipack": (api:canvas) => {return new UnipackRT(api);}
 };
---
+++ src/engine/ULP/ULP.ts
@@ -0,0 +1,107 @@
+import JSZip from "jszip";
+import type { DeviceInfoCanvas, Canvas, KeyID, ProjectInfo, ProjectRT } from "../ProjectRT";
+import type { ColorType, Color } from "../../types/color";
+import * as cbor from "cbor-web"
+
+class ULP implements ProjectRT {
+    //Data
+    api?: Canvas;
+    projectInfo: ProjectInfo = {};
+    resources:any = {};
+
+    //Runtime
+    currentChain: number = 0;
+
+    constructor(api: Canvas) {
+        this.api = api;
+    }
+
+    //Meta
+    LoadProjectFile(file: File): Promise<void> {
+        console.log(`Loading ULP ${file.name}`);
+        // console.log(file);
+        // this.api.setRGB(0, [1, 1], 255, 255, 255);
+
+        return new Promise(async (resolve, reject) => {
+            // try {
+                // console.log(this.projectInfo)
+                let zip = new JSZip();
+                let files:any = await zip.loadAsync(file).then(
+                    function (zip) {
+                        return Object.values(zip.files);
+                    },
+
+                    function (e) {
+                        throw "Failed to load selected file";
+                        console.log(e);
+                        return;
+                    }
+                );
+
+                var meta;
+                //Load projectInfo and categorize files
+                for (let file of files) {
+                    let filename = file.name.toLowerCase();
+                    if(filename.startsWith("resources/"))
+                    {
+                        this.resources[filename.split("/").pop()] = await file.async("blob")
+                    }
+                    else if(filename === "meta.uad")
+                    {
+                        meta = cbor.decode(await file.async("arraybuffer"))
+                        console.log(meta)
+                    }
+                }
+
+                console.log("Project Loaded");
+                resolve();
+            // } catch (e) {
+            //     reject(e);
+            // }
+        });
+    }
+
+    ClearProjectFile(): void { }
+
+    //Input
+    KeyPress(device: DeviceInfoCanvas, keyID: KeyID): void {
+        
+    }
+
+    KeyRelease(device: DeviceInfoCanvas, keyID: KeyID): void {
+        
+    }
+    ChainChange(chain: number): void { console.log(`Chain Change ${chain}`) }
+
+    //Autoplay
+    AutoplayStart(): void { }
+    AutoplayStop(): void { }
+    AutoplayNext(): void { }
+    AutoplayPrevious(): void { }
+    AutoplaySeek(position: number): void { }
+
+    //Info
+    GetProjectInfo(): ProjectInfo {
+        return this.projectInfo;
+    }
+    GetAutoplayProgress(): [number, number] {
+        return [0, 0];
+    }
+    GetChain(): number {
+        return 0;
+    }
+
+    //Helper
+    IndexOfKeyID(array: KeyID[], target: KeyID): number {
+        if (Array.isArray(target)) {
+            for (var k: number = 0; k < array.length; k++) {
+                if (array[k][0] == target[0] && array[k][1] == target[1]) {
+                    return k
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+export default ULP;
